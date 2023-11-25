3f1f404989504220a6d63af064303dc744ff26ce
RVPdev <renatovilcahuaman@gmail.com>
2023-11-24 20:36:45 -0600

    added boilerplate code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <raylib.h>
 
+int screenWitdh = 400;
+int screenHeigth = 400;
+
 int main()
 {
+    InitWindow(screenWitdh, screenHeigth, "Mine Sweeper");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        
+        EndDrawing();
+    }
+    
+    CloseWindow();
     return 0;
 }
