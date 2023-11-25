77d82b9b9d3313721b6394c40804922e0e66d1c3
RVPdev <renatovilcahuaman@gmail.com>
2023-11-25 17:14:03 -0600

    added comments

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,43 +1,48 @@
 #include "src/inlcude/grid.h"
 #include <iostream>
 
+// Constructor for the Grid class
 Grid::Grid()
 {
-    numCols = 15;
-    numRows = 15;
-    cellSize = 40;
-    Initialize();
+    numCols = 15;  // Set the number of columns in the grid to 15
+    numRows = 15;  // Set the number of rows in the grid to 15
+    cellSize = 40; // Set the size of each cell in the grid to 40 pixels
+    Initialize();  // Call the Initialize method to set up the grid
 }
 
+// Initialize method to set up the grid
 void Grid::Initialize()
 {
-    for (int row = 0; row < numRows; row++)
+    for (int row = 0; row < numRows; row++) // Loop through each row
     {
-        for (int col = 0; col < numCols; col++)
+        for (int col = 0; col < numCols; col++) // Loop through each column
         {
-            grid[row][col] = 0;
+            grid[row][col] = 0; // Set the value of each cell in the grid to 0
         }
     }
 }
 
+// Print method to output the grid to the console
 void Grid::Print()
 {
-    for (int row = 0; row < numRows; row++)
+    for (int row = 0; row < numRows; row++) // Loop through each row
     {
-        for (int col = 0; col < numCols; col++)
+        for (int col = 0; col < numCols; col++) // Loop through each column
         {
-            std::cout << grid[row][col] << " ";
+            std::cout << grid[row][col] << " "; // Print the value of each cell followed by a space
         }
-        std::cout << std::endl;
+        std::cout << std::endl; // Print a newline at the end of each row
     }
 }
 
+// Draw method to render the grid on the screen
 void Grid::Draw()
 {
-    for (int row = 0; row < numRows; row++)
+    for (int row = 0; row < numRows; row++) // Loop through each row
     {
-        for (int col = 0; col < numCols; col++)
+        for (int col = 0; col < numCols; col++) // Loop through each column
         {
+            // Draw a rectangle for each cell of the grid
             DrawRectangleLines(col * cellSize, row * cellSize, cellSize, cellSize, BLACK);
         }
     }
--- src/inlcude/grid.h
+++ src/inlcude/grid.h
@@ -1,18 +1,19 @@
-#pragma once
-#include <raylib.h>
+#pragma once        // Ensures that the header file is included only once during compilation
+#include <raylib.h> // Includes the Raylib library for graphics and window management
 
+// Declares the Grid class
 class Grid
-{  
-public:
-    Grid();
-    void Draw();
-    void Initialize();
-    void Print();
-    int grid[15][15];
+{
+public:                // Public members and methods are accessible from outside the class
+    Grid();            // Constructor for the Grid class. It's called when a new Grid object is created.
+    void Draw();       // Method to draw the grid on the screen. It defines how the grid will be visually represented.
+    void Initialize(); // Method to initialize or reset the grid to a default state.
+    void Print();      // Method for debugging purposes to print the grid's current state to the console or a log.
+    int grid[15][15];  // A 2D array representing the grid itself. Each element can store an integer value.
+    // The size of the grid is 15x15 cells.
 
-private:
-
-    int numRows;
-    int numCols;
-    int cellSize;
+private:          // Private members and methods are only accessible from within the class
+    int numRows;  // Variable to store the number of rows in the grid.
+    int numCols;  // Variable to store the number of columns in the grid.
+    int cellSize; // Variable to store the size of each cell in the grid. This could be used for drawing.
 };
